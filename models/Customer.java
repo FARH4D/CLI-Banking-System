363d8309e6420e6ea08ce008287f880cac291a41
FARH4D <FarhadAhmed24@hotmail.co.uk>
2023-08-01 15:38:16 +0100

    Added methods for Customer and Transaction classes

--- models/Customer.java
+++ models/Customer.java
@@ -5,9 +5,57 @@ import java.util.List;
 
 public class Customer {
     private int id;
+    private float balance;
     private String name;
     private String email;
 
     private final List<Transaction> transactions = new ArrayList<>();
-    
+
+    public Customer(int id, float balance, String name, String email){
+        this.id = id;
+        this.balance = balance;
+        this.name = name;
+        this.email = email;
+    }
+
+    public void addTransaction(Transaction transaction){
+        
+    }
+
+    public List<Transaction> getTransactions(){
+        return transactions;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
--- models/Transaction.java
+++ models/Transaction.java
@@ -1,5 +1,29 @@
 package models;
 
+import java.time.LocalDate;
+
 public class Transaction {
     
+    private Customer customer1;
+    private Customer customer2;
+    private LocalDate transactionDate;
+
+    public Transaction(Customer customer1, Customer customer2, LocalDate transactionDate){
+        this.customer1 = customer1;
+        this.customer2 = customer2;
+        this.transactionDate = transactionDate;
+    }
+
+    public Customer getCustomer1() {
+        return this.customer1;
+    }
+
+    public Customer getCustomer2() {
+        return this.customer2;
+    }
+
+    public LocalDate getTransactionDate() {
+        return this.transactionDate;
+    }
+
 }
